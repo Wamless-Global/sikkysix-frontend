e14f4a3143a2aa967cf8fc11c90c447ae2f0857b
itschu <palmerbideconcepts@gmail.com>
2025-06-01 12:54:05 +0100

    Fixed bug

--- next.config.ts
+++ next.config.ts
@@ -29,9 +29,8 @@ const nextConfig: NextConfig = {
 			{
 				// Proxy requests from /api/... on the frontend to http://localhost:5002/api/... on the backend
 				source: '/api/:path*',
-				// Ensure destination includes the /v1 path consistent with API_BASE_URL
-				destination: 'https://api.sikkysix.local/api/v1/:path*',
-				// destination: 'https://sikkysix-backend-production.up.railway.app/api/v1/:path*',
+
+				destination: `${process.env.API_BASE_URL}/:path*`,
 			},
 		];
 	},
