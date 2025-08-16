061a1d33d1e4a4ed255544c8910367c4956cb413
itschu <palmerbideconcepts@gmail.com>
2025-08-16 14:52:06 +0100

    fixed build bug

--- src/app/(protected)/admin/login-as/content.tsx
+++ src/app/(protected)/admin/login-as/content.tsx
@@ -56,7 +56,7 @@ export default function LoggingInPage() {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen">
 			<div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500 border-solid mb-4"></div>
-			<span className="text-lg font-medium text-gray-700 dark:text-gray-200">Logging you into the user's account...</span>
+			<span className="text-lg font-medium text-gray-700 dark:text-gray-200">Logging you into the user&apos;s account...</span>
 		</div>
 	);
 }
